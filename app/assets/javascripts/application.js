4cd9cdbaa1c252b94acd8617d286bb8d6bbc7e62
yixiu <aken@aken.com>
2017-06-02 15:18:26 +0800

    yixiu add button in show

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -81,3 +81,28 @@ $(document).on('mouseover', '.productDetail-left-imageList-item', function () {
   var src = $(this).find('img').attr('src') //从被鼠标选中的图片的src里面拿到图片链接
   $('.productDetail-left-bigImage').find('img').attr('src', src) //把图片链接设置到大图的src里面
 })
+
+
+//在产品页面更改商品数量
+$(document).on('turbolinks:load', function() {
+  /*增加数量*/
+  $("#quantity-plus").click(function(e) {
+    var num = parseInt($("#quantity-input").val()) + 1;
+    $("#quantity-minus").removeClass("disabled");
+    $("#quantity-input").val(num);
+    e.preventDefault();
+  });
+
+  /*减少数量*/
+  $("#quantity-minus").click(function(e) {
+    var num = parseInt($("#quantity-input").val());
+    if (num > 1) {
+      $("#quantity-input").val(num -= 1);
+      $("#quantity-plus").removeClass("disabled");
+    }
+    if (num <= 1) {
+      $("#quantity-minus").addClass("disabled");
+    }
+    e.preventDefault();
+  });
+});
