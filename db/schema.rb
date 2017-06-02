1e3a0968d3ee0d2f91d887cc4f285a908303e613
yixiu <aken@aken.com>
2017-06-02 16:45:33 +0800

    yixiu can change quantity in show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170531213301) do
+ActiveRecord::Schema.define(version: 20170602072050) do
 
   create_table "average_caches", force: :cascade do |t|
     t.integer  "rater_id"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20170531213301) do
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
     t.integer  "product_id"
-    t.integer  "quantity",   default: 1
+    t.integer  "quantity",   default: 0
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
